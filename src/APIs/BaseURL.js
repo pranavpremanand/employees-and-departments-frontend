cb0f0ab9b56866833cbbf9c60dd8d7f6647dc46f
Pranav Premanand <mpranavprem@gmail.com>
2023-03-23 16:10:04 +0530

    backend hosted

--- src/APIs/BaseURL.js
+++ src/APIs/BaseURL.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export const baseUrl = axios.create({ baseURL: "http://localhost:5000" });
+export const baseUrl = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+// export const baseUrl = axios.create({ baseURL: "http://localhost:5000" });
